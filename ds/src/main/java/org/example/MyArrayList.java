fb4bbd0de0069a08c91eb8c67d81e2938301e293
madinabolat <madinabolat01@gmail.com>
2026-02-07 22:26:51 -0500

    notes on realizing arraylist with integers

--- ds/src/main/java/org/example/MyArrayList.java
+++ ds/src/main/java/org/example/MyArrayList.java
@@ -1,12 +1,15 @@
 package org.example;
 
 public class MyArrayList {
-    int[] myArray = new int[10];
+    int default_initial_capacity = 10;
+    int[] myArray;
 
     public MyArrayList(){
+        myArray = new int[default_initial_capacity];
     }
 
-    public void add(){
-        
+    public void add(int i){
+        //check if still in bounds of default initial capacity
+        //if not - create new array and copy previous into the new one
     }
 }
