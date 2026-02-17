a8fa47e77b7107b1df5720ae35e9a52135ec09d0
madinabolat <madinabolat01@gmail.com>
2026-02-16 21:27:26 -0500

    write linkedlist add

--- ds/src/main/java/org/example/Main.java
+++ ds/src/main/java/org/example/Main.java
@@ -9,42 +9,42 @@ import java.util.Iterator;
 public class Main {
     public static void main(String[] args) {
 
-        MyArrayList myArrayList1 = new MyArrayList();
-        for (int i : myArrayList1.myArray){
-            System.out.println(i);
-        }
-
-
-        myArrayList1.add(1);
-        myArrayList1.add(2);
-        myArrayList1.add(3);
-        myArrayList1.add(4);
-        myArrayList1.add(5);
-
-        System.out.println("curr position: " + myArrayList1.currentPosition);
-        System.out.println("After adding elements");
-        int counter = 0;
-        for (int i : myArrayList1.myArray){
-            System.out.println("Element " + counter + ": " + i);
-            counter ++;
-        }
-
-        System.out.println("Size: " + myArrayList1.size());
-
-        myArrayList1.remove(0);
-        System.out.println("curr position: " + myArrayList1.currentPosition);
-
-        System.out.println("After removing");
-        int counter1 = 0;
-        for (int i : myArrayList1.myArray){
-            System.out.println("Element " + counter1 + ": " + i);
-            counter1 ++;
-        }
-        System.out.println("Size: " + myArrayList1.size());
-
-
-        System.out.println("requesting");
-        System.out.println(myArrayList1.get(2));
+//        MyArrayList myArrayList1 = new MyArrayList();
+//        for (int i : myArrayList1.myArray){
+//            System.out.println(i);
+//        }
+//
+//
+//        myArrayList1.add(1);
+//        myArrayList1.add(2);
+//        myArrayList1.add(3);
+//        myArrayList1.add(4);
+//        myArrayList1.add(5);
+//
+//        System.out.println("curr position: " + myArrayList1.currentPosition);
+//        System.out.println("After adding elements");
+//        int counter = 0;
+//        for (int i : myArrayList1.myArray){
+//            System.out.println("Element " + counter + ": " + i);
+//            counter ++;
+//        }
+//
+//        System.out.println("Size: " + myArrayList1.size());
+//
+//        myArrayList1.remove(0);
+//        System.out.println("curr position: " + myArrayList1.currentPosition);
+//
+//        System.out.println("After removing");
+//        int counter1 = 0;
+//        for (int i : myArrayList1.myArray){
+//            System.out.println("Element " + counter1 + ": " + i);
+//            counter1 ++;
+//        }
+//        System.out.println("Size: " + myArrayList1.size());
+//
+//
+//        System.out.println("requesting");
+//        System.out.println(myArrayList1.get(2));
 
 
 //        ArrayList<Integer> myAL = new ArrayList<>();
@@ -57,6 +57,13 @@ public class Main {
 //
 //        myAL.get(10);
 
+
+        MyLinkedList myLL = new MyLinkedList();
+        System.out.println(myLL);
+        myLL.add(1);
+        myLL.add(2);
+        myLL.add(3);
+        System.out.println(myLL);
 
     }
 }
--- ds/src/main/java/org/example/MyLinkedList.java
+++ ds/src/main/java/org/example/MyLinkedList.java
@@ -13,6 +13,36 @@ public class MyLinkedList {
     }
 
     Node head;
+    int size;
+
+    public void add(int n){
+        if (head == null){
+            head = new Node(n);
+        } else {
+            Node temp = head;
+            while (temp.next!=null){
+                temp = temp.next;
+            }
+            temp.next = new Node(n);
+        }
+    }
+
+    @Override
+    public String toString(){
+        String s = "[";
+        Node temp = head;
+
+        while (temp != null){
+            s += temp.value + ",";
+            temp = temp.next;
+        }
+        s = s.replaceAll(",$", "");
+        s+= "]";
+        return s;
+    }
+
+    //add to the end
+    //add to specific index
 
 
     //is the first element added to mylinkedlist is becoming its head?
